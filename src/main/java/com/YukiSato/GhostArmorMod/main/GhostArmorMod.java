40de594e9266c6453e6792bf6bade001a7e74ad4
EuropaYuki <yuki.yambaru@gmail.com>
2025-07-26 12:31:24 +0900

    テキストファイルを削除

---
+++ src/main/java/com/YukiSato/GhostArmorMod/main/GhostArmorMod.java
@@ -0,0 +1,12 @@
+package com.YukiSato.GhostArmorMod.main;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("ghostarmormod")
+public class GhostArmorMod {
+    public static final String MOD_ID = "ghostarmormod";
+
+    public GhostArmorMod() {
+
+    }
+}
