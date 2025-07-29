65fa19d4300d8730a3f12d464c123ccd2501252c
EuropaYuki <yuki.yambaru@gmail.com>
2025-07-29 16:32:05 +0900

    メインクラスのコンストラクタの中に、クリエイティブタブを登録するためのクラスをMod読み込み時に呼び出すコードを追加

--- src/main/java/com/YukiSato/GhostArmorMod/main/GhostArmorMod.java
+++ src/main/java/com/YukiSato/GhostArmorMod/main/GhostArmorMod.java
@@ -1,11 +1,16 @@
 package com.YukiSato.GhostArmorMod.main;
 
+import com.YukiSato.GhostArmorMod.regi.tab.GhostArmorModTabs;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("ghostarmormod")
 public class GhostArmorMod {
     public static final String MOD_ID = "ghostarmormod";
 
     public GhostArmorMod () {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        GhostArmorModTabs.MOD_TABS.register(bus);
     }
 }
