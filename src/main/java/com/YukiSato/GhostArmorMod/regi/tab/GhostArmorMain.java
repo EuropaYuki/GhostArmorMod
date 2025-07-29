eb62147b999225f1cc18ab72d53161c363545bb3
EuropaYuki <yuki.yambaru@gmail.com>
2025-07-29 16:42:33 +0900

    タブが立ち上がらなかったので、アイテムを追加した

--- src/main/java/com/YukiSato/GhostArmorMod/regi/tab/GhostArmorMain.java
+++ src/main/java/com/YukiSato/GhostArmorMod/regi/tab/GhostArmorMain.java
@@ -1,9 +1,10 @@
 package com.YukiSato.GhostArmorMod.regi.tab;
 
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 
 public class GhostArmorMain {
     public static final Item[] items = {
-
+            Items.DIAMOND
     };
 }
