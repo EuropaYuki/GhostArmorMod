e1a126197e1450aa36b14ee61db1aee426db22d4
EuropaYuki <yuki.yambaru@gmail.com>
2025-07-29 17:47:59 +0900

    GhostIngotをクリエイティブタブに追加

--- src/main/java/com/YukiSato/GhostArmorMod/regi/tab/GhostArmorMain.java
+++ src/main/java/com/YukiSato/GhostArmorMod/regi/tab/GhostArmorMain.java
@@ -1,10 +1,12 @@
 package com.YukiSato.GhostArmorMod.regi.tab;
 
+import com.YukiSato.GhostArmorMod.regi.GhostArmorModItems;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 
 public class GhostArmorMain {
     public static final Item[] items = {
-            Items.DIAMOND
+            Items.DIAMOND,
+            GhostArmorModItems.GHOST_INGOT.get()
     };
 }
