ba767c06d592e3c70a86756a25bd71bca9bfa89a
EuropaYuki <yuki.yambaru@gmail.com>
2025-07-29 16:19:19 +0900

    GhostArmorModのクリエイティブタブを登録するためのクラスを作成

---
+++ src/main/java/com/YukiSato/GhostArmorMod/regi/tab/GhostArmorModTabs.java
@@ -0,0 +1,27 @@
+package com.YukiSato.GhostArmorMod.regi.tab;
+
+import com.YukiSato.GhostArmorMod.main.GhostArmorMod;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class GhostArmorModTabs {
+    public static final DeferredRegister<CreativeModeTab> MOD_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, GhostArmorMod.MOD_ID);
+    public static final RegistryObject<CreativeModeTab> GHOST_ARMOR_MAIN = MOD_TABS.register("ghost_armor_main",
+            ()->{return CreativeModeTab.builder()
+                    .icon(()-> new ItemStack(Blocks.DIRT))
+                    .title(Component.translatable("itemGroup.ghost_armor_main"))
+                    .displayItems((param, output)-> {
+                        for (Item item:GhostArmorMain.items) {
+                            output.accept(item);
+                        }
+                    })
+                    .build();
+    });
+}
