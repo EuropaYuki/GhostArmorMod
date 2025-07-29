96c567657088d7d8739f475fd58626f0e6bb9a80
EuropaYuki <yuki.yambaru@gmail.com>
2025-07-29 16:58:16 +0900

    インゴットのクラスを作成

---
+++ src/main/java/com/YukiSato/GhostArmorMod/item/Inghost.java
@@ -0,0 +1,9 @@
+package com.YukiSato.GhostArmorMod.item;
+
+import net.minecraft.world.item.Item;
+
+public class Inghost extends Item {
+    public Inghost() {
+        super(new Properties().fireResistant());
+    }
+}
