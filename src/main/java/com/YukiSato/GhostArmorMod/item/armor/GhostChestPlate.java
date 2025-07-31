f58f91a9bd9ca8a56b288b8b0133f8409aa35e0a
EuropaYuki <yuki.yambaru@gmail.com>
2025-07-31 18:05:58 +0900

    防具フル装備で飛べるかどうかのエンジンを追加

--- src/main/java/com/YukiSato/GhostArmorMod/item/armor/GhostChestPlate.java
+++ src/main/java/com/YukiSato/GhostArmorMod/item/armor/GhostChestPlate.java
@@ -1,13 +1,48 @@
 package com.YukiSato.GhostArmorMod.item.armor;
 
 import com.YukiSato.GhostArmorMod.regi.GhostArmorModItems;
+import net.minecraft.ChatFormatting;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class GhostChestPlate extends ArmorItem {
     public GhostChestPlate() {
         super(GhostArmorMaterials.GHOST, Type.CHESTPLATE, new Properties().fireResistant());
+    }
+
+    private void flyMode(ItemStack stack, Player player) {
+        if (stack.getTag() == null) {
+            stack.setTag(new CompoundTag());
+        }
+        stack.getTag().putInt("canFly", fullArmor(player) ? 1 : 0);
+    }
+    public int getModeNum(ItemStack stack) {
+        if (stack.getTag() == null) {
+            return 0;
+        }
+        return stack.getTag().getInt("canFly");
+    }
+
+    private String modeName (ItemStack stack) {
+        return switch (getModeNum(stack)) {
+            case 0 -> "Disable";
+            case 1 -> "Enable";
+            default -> "Unknown";
+        };
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> list, TooltipFlag tooltipFlag) {
+        list.add(Component.literal("Fly : " + modeName(stack)).withStyle(ChatFormatting.AQUA));
     }
 
     private boolean fullArmor(Player player) {
