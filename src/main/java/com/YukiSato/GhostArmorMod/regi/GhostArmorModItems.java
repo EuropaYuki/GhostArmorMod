d934113bf8271df4d72110545456c3c6340faa8a
EuropaYuki <yuki.yambaru@gmail.com>
2025-07-31 16:53:39 +0900

    ヘルメット、チェストプレート、レギンス、ブーツをGhostArmorItemsクラスに登録

--- src/main/java/com/YukiSato/GhostArmorMod/regi/GhostArmorModItems.java
+++ src/main/java/com/YukiSato/GhostArmorMod/regi/GhostArmorModItems.java
@@ -1,7 +1,11 @@
 package com.YukiSato.GhostArmorMod.regi;
 
 import com.YukiSato.GhostArmorMod.item.GhostIngot;
+import com.YukiSato.GhostArmorMod.item.armor.GhostBoots;
+import com.YukiSato.GhostArmorMod.item.armor.GhostHelmet;
+import com.YukiSato.GhostArmorMod.item.armor.GhostLeggings;
 import com.YukiSato.GhostArmorMod.main.GhostArmorMod;
+import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -10,4 +14,8 @@ import net.minecraftforge.registries.RegistryObject;
 public class GhostArmorModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, GhostArmorMod.MOD_ID);
     public static final RegistryObject<Item> GHOST_INGOT = ITEMS.register("ghost_ingot", GhostIngot::new);
+    public static final RegistryObject<ArmorItem> GHOST_HELMET = ITEMS.register("ghost_helmet", GhostHelmet::new);
+    public static final RegistryObject<ArmorItem> GHOST_CHEST_PLATE = ITEMS.register("ghost_chest_plate", GhostHelmet::new);
+    public static final RegistryObject<ArmorItem> GHOST_LEGGINGS = ITEMS.register("ghost_leggings", GhostLeggings::new);
+    public static final RegistryObject<ArmorItem> GHOST_BOOTS = ITEMS.register("ghost_boots", GhostBoots::new);
 }
