0af2c185c7d385fa3f71724a27eac1130a5d994a
EuropaYuki <yuki.yambaru@gmail.com>
2025-07-31 16:56:09 +0900

    ヘルメット、チェストプレート、レギンス、ブーツをクリエイティブタブに追加

--- src/main/java/com/YukiSato/GhostArmorMod/regi/tab/GhostArmorMain.java
+++ src/main/java/com/YukiSato/GhostArmorMod/regi/tab/GhostArmorMain.java
@@ -7,6 +7,10 @@ import net.minecraft.world.item.Items;
 public class GhostArmorMain {
     public static final Item[] items = {
             Items.DIAMOND,
-            GhostArmorModItems.GHOST_INGOT.get()
+            GhostArmorModItems.GHOST_INGOT.get(),
+            GhostArmorModItems.GHOST_HELMET.get(),
+            GhostArmorModItems.GHOST_CHEST_PLATE.get(),
+            GhostArmorModItems.GHOST_LEGGINGS.get(),
+            GhostArmorModItems.GHOST_BOOTS.get()
     };
 }
