ccc437d2ec5399565407b2889139c8bbc6f1b074
EuropaYuki <yuki.yambaru@gmail.com>
2025-07-31 17:03:20 +0900

    チェストプレートの参照クラスが、間違っていたため、修正

--- src/main/java/com/YukiSato/GhostArmorMod/regi/GhostArmorModItems.java
+++ src/main/java/com/YukiSato/GhostArmorMod/regi/GhostArmorModItems.java
@@ -2,6 +2,7 @@ package com.YukiSato.GhostArmorMod.regi;
 
 import com.YukiSato.GhostArmorMod.item.GhostIngot;
 import com.YukiSato.GhostArmorMod.item.armor.GhostBoots;
+import com.YukiSato.GhostArmorMod.item.armor.GhostChestPlate;
 import com.YukiSato.GhostArmorMod.item.armor.GhostHelmet;
 import com.YukiSato.GhostArmorMod.item.armor.GhostLeggings;
 import com.YukiSato.GhostArmorMod.main.GhostArmorMod;
@@ -15,7 +16,7 @@ public class GhostArmorModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, GhostArmorMod.MOD_ID);
     public static final RegistryObject<Item> GHOST_INGOT = ITEMS.register("ghost_ingot", GhostIngot::new);
     public static final RegistryObject<ArmorItem> GHOST_HELMET = ITEMS.register("ghost_helmet", GhostHelmet::new);
-    public static final RegistryObject<ArmorItem> GHOST_CHEST_PLATE = ITEMS.register("ghost_chest_plate", GhostHelmet::new);
+    public static final RegistryObject<ArmorItem> GHOST_CHEST_PLATE = ITEMS.register("ghost_chest_plate", GhostChestPlate::new);
     public static final RegistryObject<ArmorItem> GHOST_LEGGINGS = ITEMS.register("ghost_leggings", GhostLeggings::new);
     public static final RegistryObject<ArmorItem> GHOST_BOOTS = ITEMS.register("ghost_boots", GhostBoots::new);
 }
